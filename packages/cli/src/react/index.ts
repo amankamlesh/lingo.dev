c1bb6c20860eb20879eef98c2691e5f1c359d540
Aman Mahor <aman.seema1976@gmail.com>
2025-11-04 11:44:55 +0530

    resolving cli tsconfig.json

--- packages/cli/src/react/index.ts
+++ packages/cli/src/react/index.ts
@@ -1,3 +1,5 @@
 // Re-export everything but with type checking
+export type * from "../react/";
+export * from "../react";
 export type * from "@lingo.dev/_react";
 export * from "@lingo.dev/_react";
